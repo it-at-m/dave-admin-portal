39afdd9d565c5076a53f6bc919e900a3b522a444
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-11-17 15:14:08 +0100

    type entfernt, da in oberklasse definiert

--- frontend/src/domain/dto/MessstelleKarteDTO.ts
+++ frontend/src/domain/dto/MessstelleKarteDTO.ts
@@ -3,7 +3,5 @@ import TooltipMessstelleDTO from "@/domain/dto/TooltipMessstelleDTO";
 import AnzeigeKarteDTO from "@/domain/dto/AnzeigeKarteDTO";
 
 export default interface MessstelleKarteDTO extends AnzeigeKarteDTO {
-    type: string;
     tooltip: TooltipMessstelleDTO;
 }
-
