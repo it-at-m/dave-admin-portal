8dfe6421b7d9352c335a36691fc14854619dd054
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-11-17 13:30:11 +0100

    suche bekommt parent zurück / testdaten hinzugefügt

--- frontend/src/api/service/SucheService.ts
+++ frontend/src/api/service/SucheService.ts
@@ -1,6 +1,8 @@
 import FetchService from "@/api/service/FetchService";
 import SucheComplexSuggestsDTO from "@/domain/dto/suche/SucheComplexSuggestsDTO";
 import ZaehlstelleKarteDTO from "@/domain/dto/ZaehlstelleKarteDTO";
+import MessstelleKarteDTO from "@/domain/dto/MessstelleKarteDTO";
+import AnzeigeKarteDTO from "@/domain/dto/AnzeigeKarteDTO";
 
 export default class SucheService {
     private static readonly ENDPOINT: string = "api/dave-backend-service";
@@ -14,12 +16,61 @@ export default class SucheService {
         );
     }
 
-    static searchZaehlstelle(
-        query: string
-    ): Promise<Array<ZaehlstelleKarteDTO>> {
+    static searchZaehlstelle(query: string): Promise<Array<AnzeigeKarteDTO>> {
         return FetchService.getData(
             `${this.ENDPOINT}/search?nofilter=true&query=${query}`,
             "Beim Suchen von Zählstellen ist ein Fehler aufgetreten."
         );
+    }
+    static getMockMessstelleKarte(): AnzeigeKarteDTO[] {
+        const mstOne: MessstelleKarteDTO = {
+            type: "messstelle",
+            id: "randomID",
+            latitude: 48.1320322,
+            longitude: 11.6242116,
+            tooltip: {
+                mstId: "MSTID1",
+                standortDatenportal: "testdatenportal",
+                stadtbezirk: 13,
+                realisierungsdatum: "testrealisierung",
+                abbaudatum: "testabbau",
+                datumLetzteMessung: "testmessung",
+                kfz: true,
+            },
+        };
+
+        const msttwo: MessstelleKarteDTO = {
+            type: "messstelle",
+            id: "randomID2",
+            latitude: 48.1316995,
+            longitude: 11.6227731,
+            tooltip: {
+                mstId: "MSTID2",
+                standortDatenportal: "testdatenportal2",
+                stadtbezirk: 13,
+                realisierungsdatum: "testrealisierung2",
+                abbaudatum: "testabbau2",
+                datumLetzteMessung: "testmessung2",
+                kfz: true,
+            },
+        };
+
+        const mstthree: MessstelleKarteDTO = {
+            type: "messstelle",
+            id: "randomID",
+            latitude: 48.1315995,
+            longitude: 11.6217731,
+            tooltip: {
+                mstId: "MSTID3",
+                standortDatenportal: "testdatenportal3",
+                stadtbezirk: 13,
+                realisierungsdatum: "testrealisierung3",
+                abbaudatum: "testabbau3",
+                datumLetzteMessung: "testmessung3",
+                kfz: true,
+            },
+        };
+
+        return [mstOne, msttwo, mstthree];
     }
 }
