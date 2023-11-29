1d9e9f69de98eb60f5e1d0bd93d3568c04037719
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-11-29 14:26:30 +0100

    Feat/dave 118 ubersichtskarte fuer kfz detektordaten (#22)
    * parent für zaehlstelle und messstelle karte
    
    * suche bekommt parent zurück / testdaten hinzugefügt
    
    * Messtelle hinzugefügt
    
    * attribute in parent ausgegliedert
    
    * neue marker hinzugefügt / anpassung an anzeitekarteDTO
    
    * latlang input parameter auf number geändert
    
    * default lat/long auf 0 geändert
    
    * type entfernt, da in oberklasse definiert
    
    * typecast hinzugefügt
    
    * testdaten angepasst
    
    * tooltip messtelle angepasst
    
    * unused css class entfernt
    
    * markericons als png angelegt
    
    ---------
    
    Co-authored-by: alexander.kerscher <alexander.kerscher@muenchen.de>

--- frontend/src/api/service/SucheService.ts
+++ frontend/src/api/service/SucheService.ts
@@ -1,6 +1,8 @@
 import FetchService from "@/api/service/FetchService";
 import SucheComplexSuggestsDTO from "@/domain/dto/suche/SucheComplexSuggestsDTO";
 import ZaehlstelleKarteDTO from "@/domain/dto/ZaehlstelleKarteDTO";
+import MessstelleKarteDTO from "@/domain/dto/MessstelleKarteDTO";
+import AnzeigeKarteDTO from "@/domain/dto/AnzeigeKarteDTO";
 
 export default class SucheService {
     private static readonly ENDPOINT: string = "api/dave-backend-service";
@@ -14,12 +16,61 @@ export default class SucheService {
         );
     }
 
-    static searchZaehlstelle(
-        query: string
-    ): Promise<Array<ZaehlstelleKarteDTO>> {
+    static searchZaehlstelle(query: string): Promise<Array<AnzeigeKarteDTO>> {
         return FetchService.getData(
             `${this.ENDPOINT}/search?nofilter=true&query=${query}`,
             "Beim Suchen von Zählstellen ist ein Fehler aufgetreten."
         );
+    }
+    static getMockMessstelleKarte(): AnzeigeKarteDTO[] {
+        const mstOne: MessstelleKarteDTO = {
+            type: "messstelle",
+            id: "randomID",
+            latitude: 48.1320322,
+            longitude: 11.6242116,
+            tooltip: {
+                mstId: "MSTID1",
+                standortDatenportal: "StandortDatenportal1",
+                stadtbezirk: 13,
+                realisierungsdatum: "01.01.2023",
+                abbaudatum: "01.02.2023",
+                datumLetzteMessung: "15.01.2023",
+                kfz: true,
+            },
+        };
+
+        const msttwo: MessstelleKarteDTO = {
+            type: "messstelle",
+            id: "randomID2",
+            latitude: 48.1316995,
+            longitude: 11.6227731,
+            tooltip: {
+                mstId: "MSTID2",
+                standortDatenportal: "StandortDatenportal2",
+                stadtbezirk: 13,
+                realisierungsdatum: "02.01.2020",
+                abbaudatum: "04.04.2020",
+                datumLetzteMessung: "31.03.2020",
+                kfz: true,
+            },
+        };
+
+        const mstthree: MessstelleKarteDTO = {
+            type: "messstelle",
+            id: "randomID",
+            latitude: 48.1315995,
+            longitude: 11.6217731,
+            tooltip: {
+                mstId: "MSTID3",
+                standortDatenportal: "StandortDatenportal3",
+                stadtbezirk: 13,
+                realisierungsdatum: "01.01.2023",
+                abbaudatum: "01.01.2023",
+                datumLetzteMessung: "01.03.2023",
+                kfz: true,
+            },
+        };
+
+        return [mstOne, msttwo, mstthree];
     }
 }
