281731b901e02466fe200efe9a14078f710659c7
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-12-04 10:45:13 +0100

    dummy datei hinzugefügt

--- frontend/src/api/service/MessstelleService.ts
+++ frontend/src/api/service/MessstelleService.ts
@@ -1,9 +1,18 @@
-import ZaehlstelleDTO from "@/domain/dto/ZaehlstelleDTO";
-import FetchService from "@/api/service/FetchService";
+import MessstelleDTO from "@/domain/dto/MessstelleDTO";
 
 export default class MessstelleService {
-
-    static getMessstelleById(id: string): Promise<ZaehlstelleDTO> {
-
+    static getMessstelleById(id: string): MessstelleDTO {
+        return {
+            createdTime: "heute",
+            entityVersion: 0,
+            id: "id",
+            name: "NameMesstelle",
+            stadtbezirkNummer: 12,
+            lat: 48.1317995,
+            lng: 11.6228731,
+            customSuchwoerter: ["suchwort1", "suchwort2"],
+            kommentar: "string",
+            sichtbarDatenportal: true,
+        };
     }
 }
