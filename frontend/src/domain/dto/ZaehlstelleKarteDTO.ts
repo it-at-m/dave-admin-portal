598e6404780d27ae83e30a7f0fe4cdc1a907c32c
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-12-04 18:02:37 +0100

    merge fehler entfernt

--- frontend/src/domain/dto/ZaehlstelleKarteDTO.ts
+++ frontend/src/domain/dto/ZaehlstelleKarteDTO.ts
@@ -2,7 +2,6 @@ import TooltipDTO from "@/domain/dto/TooltipDTO";
 import AnzeigeKarteDTO from "@/domain/dto/AnzeigeKarteDTO";
 
 export default interface ZaehlstelleKarteDTO extends AnzeigeKarteDTO {
-
     nummer: string;
 
     letzteZaehlungId: string;
