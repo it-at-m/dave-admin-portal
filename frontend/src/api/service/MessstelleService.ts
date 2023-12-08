1e63bbef77476e21bdac7491d2775a317f2902a4
DanielOber <145556033+DanielOber@users.noreply.github.com>
2023-12-08 11:32:53 +0100

    Feat/dave 119 einzelmessstelle adminportal (#24)
    * parent für zaehlstelle und messstelle karte
    
    * suche bekommt parent zurück / testdaten hinzugefügt
    
    * Messtelle hinzugefügt
    
    * attribute in parent ausgegliedert
    
    * neue marker hinzugefügt / anpassung an anzeitekarteDTO
    
    * latlang input parameter auf number geändert
    
    * default lat/long auf 0 geändert
    
    * type entfernt, da in oberklasse definiert
    
    * typecast hinzugefügt
    
    * testdaten angepasst
    
    * tooltip messtelle angepasst
    
    * unused css class entfernt
    
    * erste übresicht für messstelle erstellt
    
    * dummy datei hinzugefügt
    
    * marker click hinzugefügt
    
    * datei um auf vuetify zuzugreifen
    
    * anpassungen für die koordinate
    
    * neue dummydaten hinzugefügt
    
    * mstid hinzugefügt
    
    * heigt und minheigt ergänzt
    
    * ref und params angepasst
    
    * testdaten entfernt
    
    * id angepasst
    
    * karte attribution angepasst
    
    * private entfernt
    
    * merge fehler entfernt
    
    * merge fehler entfernt
    
    * fixed typo
    
    * reihenfolge script / Template geändert
    
    * änderung concated string zu template literal
    
    Co-authored-by: Der-Alex-K <18263366+Der-Alex-K@users.noreply.github.com>
    
    * änderung concated string zu template literal
    
    Co-authored-by: Der-Alex-K <18263366+Der-Alex-K@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Der-Alex-K <18263366+Der-Alex-K@users.noreply.github.com>

---
+++ frontend/src/api/service/MessstelleService.ts
@@ -0,0 +1,64 @@
+import MessstelleDTO from "@/domain/dto/MessstelleDTO";
+
+export default class MessstelleService {
+    static getMessstelleById(id: string): MessstelleDTO {
+        switch (id) {
+            case "randomID":
+                return {
+                    createdTime: "heute",
+                    entityVersion: 0,
+                    id: "randomID",
+                    mstId: "MSTID1",
+                    name: "Messstelle 1",
+                    stadtbezirkNummer: 13,
+                    lat: 48.1320322,
+                    lng: 11.6242116,
+                    customSuchwoerter: ["suchwort1", "suchwort2"],
+                    kommentar: "string",
+                    sichtbarDatenportal: true,
+                };
+            case "randomID2":
+                return {
+                    createdTime: "heute",
+                    entityVersion: 0,
+                    mstId: "MSTID2",
+                    id: "randomID2",
+                    name: "Messstelle 2",
+                    stadtbezirkNummer: 13,
+                    lat: 48.1316995,
+                    lng: 11.6227731,
+                    customSuchwoerter: ["suchwort1", "suchwort2"],
+                    kommentar: "string",
+                    sichtbarDatenportal: true,
+                };
+            case "randomID3":
+                return {
+                    createdTime: "heute",
+                    entityVersion: 0,
+                    id: "randomID3",
+                    mstId: "MSTID3",
+                    name: "Messstelle 3",
+                    stadtbezirkNummer: 13,
+                    lat: 48.1315995,
+                    lng: 11.6217731,
+                    customSuchwoerter: ["suchwort1", "suchwort2"],
+                    kommentar: "string",
+                    sichtbarDatenportal: true,
+                };
+            default:
+                return {
+                    createdTime: "error",
+                    entityVersion: 0,
+                    id: "error",
+                    mstId: "error",
+                    name: "error",
+                    stadtbezirkNummer: 13,
+                    lat: 48.1315995,
+                    lng: 11.6217731,
+                    customSuchwoerter: ["error", "error2"],
+                    kommentar: "error",
+                    sichtbarDatenportal: true,
+                };
+        }
+    }
+}
---
+++ frontend/src/components/MessstelleInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-sheet
+        width="100%"
+        color="transparent"
+        class="pa-4"
+        :min-height="props.minheight"
+        :height="props.height"
+    >
+        <span class="text-caption">Messstelle</span><br />
+        <span class="text-h5">{{ props.name }}</span
+        ><br /><br />
+        <div class="text-caption text-truncate">{{ props.id }}</div>
+        <span class="text-caption"
+            >Stadtbezirksnummer {{ props.stadtbezirkNummer }}</span
+        >
+    </v-sheet>
+</template>
+<script setup lang="ts">
+interface Props {
+    name: string;
+    id: string;
+    stadtbezirkNummer: number;
+    height: string;
+    minheight: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
