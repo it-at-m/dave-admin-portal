f1f5d5e6346e4723a455cbde016c950f0cc625f7
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-03-21 11:52:06 +0100

    umgestellt auf composition-api

--- frontend/src/components/config/subconfig/ConfigEmailAddress.vue
+++ frontend/src/components/config/subconfig/ConfigEmailAddress.vue
@@ -9,7 +9,7 @@
             :height="tableHeight"
             dense
             :headers="header"
-            :items="getEmailAddresses"
+            :items="emailaddresses"
             :items-per-page="-1"
             hide-default-footer
             fixed-header
@@ -149,245 +149,237 @@
     </v-sheet>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-/* eslint-disable no-unused-vars */
+<script setup lang="ts">
+import { useSnackbarStore } from "@/store/SnackbarStore";
+import { computed, onMounted, ref } from "vue";
+import DefaultObjectCreator from "@/util/DefaultObjectCreator";
 import EmailAddressDTO from "@/domain/dto/EmailAddressDTO";
 import EmailAddressService from "@/api/service/EmailAddressService";
-import DefaultObjectCreator from "@/util/DefaultObjectCreator";
-import _ from "lodash";
-import { useSnackbarStore } from "@/store/SnackbarStore";
-/* eslint-enable no-unused-vars */
+import { cloneDeep, isEmpty, isNull } from "lodash";
 
-@Component
-export default class ConfigEmailAddress extends Vue {
-    @Prop() readonly height!: string;
+interface Props {
+    height: string;
+}
+const props = defineProps<Props>();
+const snackbarStore = useSnackbarStore();
 
-    private snackbarStore = useSnackbarStore();
+const showEditDialog = ref(false);
+const showDeleteDialog = ref(false);
+const filterEmailaddress = ref("");
+const editIndex = ref(-1);
+const mailaddressesForDuplicateCheck = ref<Set<string>>(new Set<string>());
+const editEmailaddress = ref(
+    DefaultObjectCreator.createDefaultEmailaddressDTO()
+);
+const emailaddresses = ref<Array<EmailAddressDTO>>([]);
 
-    showEditDialog = false;
-    showDeleteDialog = false;
-    filterEmailaddress = "";
-    private editIndex = -1;
-    private mailaddressesForDuplicateCheck: Set<string> = new Set<string>();
-    editEmailaddress: EmailAddressDTO =
-        DefaultObjectCreator.createDefaultEmailaddressDTO();
-    private emailaddresses: Array<EmailAddressDTO> = [];
+onMounted(() => {
+    loadMobilitaetsreferatEmailAddress();
+});
 
-    mounted() {
-        this.loadMobilitaetsreferatEmailAddress();
-    }
+/* Lädt die Email-Adresse vom Referat */
+function loadMobilitaetsreferatEmailAddress() {
+    EmailAddressService.getEmailAddresses()
+        .then((result: Array<EmailAddressDTO>) => {
+            emailaddresses.value = [];
+            mailaddressesForDuplicateCheck.value = new Set<string>();
+            editEmailaddress.value =
+                DefaultObjectCreator.createDefaultEmailaddressDTO();
+            if (!isNull(result) && result.length > 0) {
+                emailaddresses.value = cloneDeep(result);
+                emailaddresses.value.forEach((value) => {
+                    mailaddressesForDuplicateCheck.value.add(
+                        value.emailAddress
+                    );
+                });
+            }
+        })
+        .catch((error) => snackbarStore.showApiError(error));
+}
 
-    /* Lädt die Email-Adresse vom Referat */
-    private loadMobilitaetsreferatEmailAddress() {
-        EmailAddressService.getEmailAddresses()
-            .then((result: Array<EmailAddressDTO>) => {
-                this.emailaddresses = [];
-                this.mailaddressesForDuplicateCheck = new Set<string>();
-                this.editEmailaddress =
-                    DefaultObjectCreator.createDefaultEmailaddressDTO();
-                if (result !== null && result.length > 0) {
-                    this.emailaddresses = _.cloneDeep(result);
-                    this.emailaddresses.forEach((value) => {
-                        this.mailaddressesForDuplicateCheck.add(
-                            value.emailAddress
-                        );
-                    });
-                }
+// Löschen
+/**
+ * Öffnet den Löschdialog.
+ * Ermittelt den Index des zu löschenden Items.
+ *
+ * @param item das Item was gelöscht werden soll.
+ */
+function deleteItem(item: EmailAddressDTO) {
+    editIndex.value = emailaddresses.value.indexOf(item);
+    editEmailaddress.value = cloneDeep(item);
+    showDeleteDialog.value = true;
+}
+
+/**
+ * Löscht das gewählte Item bei Klick auf den Button Löschen im Löschdialog
+ * mit einen DELETE-Request an das Backend.
+ *
+ * Nach Ausführung des Requests an das Backend werden die Daten neu vom Backend geladen.
+ */
+function deleteItemConfirm() {
+    if (editIndex.value > -1 && editEmailaddress.value) {
+        EmailAddressService.delete(editEmailaddress.value)
+            .then(() => {
+                snackbarStore.showSuccess(
+                    "Gelöscht",
+                    "Die E-Mail-Adresse wurde erfolgreich gelöscht."
+                );
             })
-            .catch((error) => this.snackbarStore.showApiError(error));
+            .catch((error) => snackbarStore.showApiError(error))
+            .finally(() => {
+                loadMobilitaetsreferatEmailAddress();
+            });
     }
+    closeDelete();
+}
 
-    // Löschen
-    /**
-     * Öffnet den Löschdialog.
-     * Ermittelt den Index des zu löschenden Items.
-     *
-     * @param item das Item was gelöscht werden soll.
-     */
-    deleteItem(item: EmailAddressDTO) {
-        this.editIndex = this.emailaddresses.indexOf(item);
-        this.editEmailaddress = Object.assign({}, item);
-        this.showDeleteDialog = true;
+/**
+ * Schließt den Löschdialog und setzt den Index des gelöschten Items zurück.
+ */
+function closeDelete() {
+    showDeleteDialog.value = false;
+    editIndex.value = -1;
+    editEmailaddress.value =
+        DefaultObjectCreator.createDefaultEmailaddressDTO();
+}
+
+// Editieren/Speichern
+/**
+ * Öffnet den Dialog zum Ändern eines items.
+ * @param item zum Ändern.
+ */
+function editItem(item: EmailAddressDTO) {
+    editIndex.value = emailaddresses.value.indexOf(item);
+    editEmailaddress.value = cloneDeep(item);
+    mailaddressesForDuplicateCheck.value.delete(item.emailAddress);
+    showEditDialog.value = true;
+}
+
+// Speichern
+/**
+ * Speichert das geänderte oder neu angelegte Item.
+ *
+ * Beim Erstellen eines neuen Items wird ein POST-Request an das Backend durchgeführt.
+ * Beim Ändern eines bestehenden Items wird ein PUT-Request an das Backend durchgeführt.
+ *
+ * Nach Ausführung des Requests an das Backend werden die Daten neu vom Backend geladen.
+ */
+function saveEditItemDialog() {
+    if (editIndex.value > -1 && editEmailaddress.value) {
+        // Bestehende Email
+        EmailAddressService.update(editEmailaddress.value)
+            .then(() => {
+                snackbarStore.showSuccess(
+                    "Aktualisiert",
+                    "Die E-Mail-Adresse wurde erfolgreich aktualisiert."
+                );
+            })
+            .catch((error) => snackbarStore.showApiError(error))
+            .finally(() => {
+                loadMobilitaetsreferatEmailAddress();
+            });
+    } else if (editEmailaddress.value) {
+        // Neue Email
+        EmailAddressService.save(editEmailaddress.value)
+            .then(() => {
+                snackbarStore.showSuccess(
+                    "Gespeichert",
+                    "Die E-Mail-Addresse wurde erfolgreich gespeichert."
+                );
+            })
+            .catch((error) => snackbarStore.showApiError(error))
+            .finally(() => {
+                loadMobilitaetsreferatEmailAddress();
+            });
     }
+    closeEditItemDialog();
+}
 
-    /**
-     * Löscht das gewählte Item bei Klick auf den Button Löschen im Löschdialog
-     * mit einen DELETE-Request an das Backend.
-     *
-     * Nach Ausführung des Requests an das Backend werden die Daten neu vom Backend geladen.
-     */
-    deleteItemConfirm() {
-        if (this.editIndex > -1 && this.editEmailaddress) {
-            EmailAddressService.delete(this.editEmailaddress)
-                .then(() => {
-                    this.snackbarStore.showSuccess(
-                        "Gelöscht",
-                        "Die E-Mail-Adresse wurde erfolgreich gelöscht."
-                    );
-                })
-                .catch((error) => this.snackbarStore.showApiError(error))
-                .finally(() => {
-                    this.loadMobilitaetsreferatEmailAddress();
-                });
-        }
-        this.closeDelete();
+/**
+ * Schließt den Editdialog und setzt den Index des geänderten Items zurück.
+ */
+function closeEditItemDialog() {
+    showEditDialog.value = false;
+    editIndex.value = -1;
+    editEmailaddress.value =
+        DefaultObjectCreator.createDefaultEmailaddressDTO();
+}
+
+// Von der Sheet-Height alles abziehen, was nicht die Tabelle ist
+// 64px Suche in Tabelle
+// 20px Padding Bottom
+// 52px Button
+const tableHeight = computed(() => {
+    return parseInt(props.height.replace("px", "")) - 136 + "px";
+});
+
+const header = [
+    {
+        text: "E-Mail-Adressen",
+        align: "start",
+        sortable: true,
+        value: "emailAddress",
+        divider: true,
+    },
+    {
+        text: "Aktionen",
+        align: "center",
+        sortable: false,
+        filterable: false,
+        value: "aktionen",
+        width: "10%",
+    },
+];
+
+const disableSpeicherButton = computed(() => {
+    const mail: string = editEmailaddress.value.emailAddress;
+
+    return (
+        isEmpty(mail) ||
+        mailaddressesForDuplicateCheck.value.has(mail) ||
+        mail.length === 0 ||
+        !isEmailValid(mail)
+    );
+});
+
+const getTitle = computed(() => {
+    return editIndex.value === -1
+        ? "E-Mail-Adresse anlegen"
+        : "E-Mail-Adresse bearbeiten";
+});
+
+/* Rules */
+
+/* Prüft, ob ein Wert gesetzt ist. */
+function pflichtfeld(value: string): boolean | string {
+    if (!isEmpty(value)) {
+        return true;
     }
+    return "Hierbei handelt es sich um ein Pflichtfeld. Bitte ausfüllen";
+}
 
-    /**
-     * Schließt den Löschdialog und setzt den Index des gelöschten Items zurück.
-     */
-    closeDelete() {
-        this.showDeleteDialog = false;
-        this.editIndex = -1;
-        this.editEmailaddress =
-            DefaultObjectCreator.createDefaultEmailaddressDTO();
+/* Prüft,ob die Email valide oder nicht leer ist*/
+function isEmailValidOrEmpty(email: string): boolean | string {
+    if (isEmpty(email) || isEmailValid(email)) {
+        return true;
+    } else {
+        return "Die Email-Adresse ist nicht valide.";
     }
+}
 
-    // Editieren/Speichern
-    /**
-     * Öffnet den Dialog zum Ändern eines items.
-     * @param item zum Ändern.
-     */
-    editItem(item: EmailAddressDTO) {
-        this.editIndex = this.emailaddresses.indexOf(item);
-        this.editEmailaddress = Object.assign({}, item);
-        this.mailaddressesForDuplicateCheck.delete(item.emailAddress);
-        this.showEditDialog = true;
+/* Prüft, ob die EMail bereits hinterlegt ist. */
+function isEmailADuplicate(email: string): boolean | string {
+    if (mailaddressesForDuplicateCheck.value.has(email)) {
+        return "Die Email-Adresse exisitert bereits.";
+    } else {
+        return false;
     }
+}
 
-    // Speichern
-    /**
-     * Speichert das geänderte oder neu angelegte Item.
-     *
-     * Beim Erstellen eines neuen Items wird ein POST-Request an das Backend durchgeführt.
-     * Beim Ändern eines bestehenden Items wird ein PUT-Request an das Backend durchgeführt.
-     *
-     * Nach Ausführung des Requests an das Backend werden die Daten neu vom Backend geladen.
-     */
-    saveEditItemDialog() {
-        if (this.editIndex > -1 && this.editEmailaddress) {
-            // Bestehende Email
-            EmailAddressService.update(this.editEmailaddress)
-                .then(() => {
-                    this.snackbarStore.showSuccess(
-                        "Aktualisiert",
-                        "Die E-Mail-Adresse wurde erfolgreich aktualisiert."
-                    );
-                })
-                .catch((error) => this.snackbarStore.showApiError(error))
-                .finally(() => {
-                    this.loadMobilitaetsreferatEmailAddress();
-                });
-        } else if (this.editEmailaddress) {
-            // Neue Email
-            EmailAddressService.save(this.editEmailaddress)
-                .then(() => {
-                    this.snackbarStore.showSuccess(
-                        "Gespeichert",
-                        "Die E-Mail-Addresse wurde erfolgreich gespeichert."
-                    );
-                })
-                .catch((error) => this.snackbarStore.showApiError(error))
-                .finally(() => {
-                    this.loadMobilitaetsreferatEmailAddress();
-                });
-        }
-        this.closeEditItemDialog();
-    }
-
-    /**
-     * Schließt den Editdialog und setzt den Index des geänderten Items zurück.
-     */
-    closeEditItemDialog() {
-        this.showEditDialog = false;
-        this.editIndex = -1;
-        this.editEmailaddress =
-            DefaultObjectCreator.createDefaultEmailaddressDTO();
-    }
-
-    // Von der Sheet-Height alles abziehen, was nicht die Tabelle ist
-    // 64px Suche in Tabelle
-    // 20px Padding Bottom
-    // 52px Button
-    get tableHeight(): string {
-        return parseInt(this.height.replace("px", "")) - 136 + "px";
-    }
-
-    get header(): Array<any> {
-        return [
-            {
-                text: "E-Mail-Adressen",
-                align: "start",
-                sortable: true,
-                value: "emailAddress",
-                divider: true,
-            },
-            {
-                text: "Aktionen",
-                align: "center",
-                sortable: false,
-                filterable: false,
-                value: "aktionen",
-                width: "10%",
-            },
-        ];
-    }
-
-    get getEmailAddresses(): Array<EmailAddressDTO> {
-        return this.emailaddresses;
-    }
-
-    get disableSpeicherButton(): boolean {
-        const mail: string = this.editEmailaddress.emailAddress;
-
-        return (
-            _.isEmpty(mail) ||
-            this.mailaddressesForDuplicateCheck.has(mail) ||
-            mail.length === 0 ||
-            !this.isEmailValid(mail)
-        );
-    }
-
-    get getTitle() {
-        return this.editIndex === -1
-            ? "E-Mail-Adresse anlegen"
-            : "E-Mail-Adresse bearbeiten";
-    }
-
-    /* Rules */
-
-    /* Prüft, ob ein Wert gesetzt ist. */
-    pflichtfeld(value: any): boolean | string {
-        if (!_.isEmpty(value)) {
-            return true;
-        }
-        return "Hierbei handelt es sich um ein Pflichtfeld. Bitte ausfüllen";
-    }
-
-    /* Prüft,ob die Email valide oder nicht leer ist*/
-    isEmailValidOrEmpty(email: string): boolean | string {
-        if (_.isEmpty(email) || this.isEmailValid(email)) {
-            return true;
-        } else {
-            return "Die Email-Adresse ist nicht valide.";
-        }
-    }
-
-    /* Prüft, ob die EMail bereits hinterlegt ist. */
-    isEmailADuplicate(email: string): boolean | string {
-        if (this.mailaddressesForDuplicateCheck.has(email)) {
-            return "Die Email-Adresse exisitert bereits.";
-        } else {
-            return false;
-        }
-    }
-
-    /* Prüft, ob eine Email valide ist.  */
-    private isEmailValid(email: string): boolean {
-        const pattern =
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return pattern.test(email);
-    }
+/* Prüft, ob eine Email valide ist.  */
+function isEmailValid(email: string): boolean {
+    const pattern =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return pattern.test(email);
 }
 </script>
