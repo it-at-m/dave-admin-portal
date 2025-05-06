e70e2bc1c1fbd3c88168b0471879b3808f0ab845
martind260 <99957516+martind260@users.noreply.github.com>
2025-05-06 14:48:38 +0200

    Configure map center and zoom (#151)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <zaehlstelle-map
       height="100%"
       width="100%"
-      :zoom="12"
+      :zoom="mapConfigStore.getMapConfig.zoom"
     />
   </v-main>
 </template>
@@ -12,6 +12,9 @@
 import { onMounted } from "vue";
 
 import ZaehlstelleMap from "@/components/map/ZaehlstelleMap.vue";
+import { useMapConfigStore } from "@/store/MapConfigStore";
+
+const mapConfigStore = useMapConfigStore();
 
 onMounted(() => {
   window.scrollTo(0, 0);
