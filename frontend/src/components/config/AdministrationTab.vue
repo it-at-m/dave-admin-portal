0911429d94d350e24c6ac89a9586f01be4daa512
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-05-28 12:56:15 +0200

    add reload for unauffaelliger tag

---
+++ frontend/src/components/config/AdministrationTab.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-sheet
+    width="100%"
+    :height="height"
+    :max-height="height"
+    class="overflow-y-auto overflow-x-hidden"
+  >
+    <v-row dense>
+      <v-col cols="3">
+        <reload-unauffaelliger-tag-card :max-width="MAX_CARD_WIDTH" />
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+import ReloadUnauffaelligerTagCard from "@/components/administration/ReloadUnauffaelligerTagCard.vue";
+
+interface Props {
+  height: string;
+}
+defineProps<Props>();
+
+const MAX_CARD_WIDTH = "374";
+</script>
