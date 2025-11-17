c91feda9b1cd3341f6cb8f48cf71299c72f6036e
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2025-11-17 13:16:59 +0100

    include art icon to avoid multiple adoptions when the icon format ist changing

--- frontend/src/components/icons/ArtIcons/ArtR.vue
+++ frontend/src/components/icons/ArtIcons/ArtR.vue
@@ -1,19 +1,9 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
-  >
-    <text
-      x="16"
-      y="26"
-      class="svg-text"
-      text-anchor="middle"
-      fill="currentColor"
-    >
-      R
-    </text>
-  </svg>
+  <art-icon model-value="R" />
 </template>
+<script setup lang="ts">
+import ArtIcon from "@/components/icons/ArtIcons/ArtIcon.vue";
+</script>
 <style lang="css">
 .svg-text {
   font: bold 23px Arial;
