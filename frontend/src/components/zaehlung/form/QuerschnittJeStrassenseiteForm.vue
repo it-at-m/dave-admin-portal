cc9da794e472cfcc36852d64d16ea43f7749171d
alexander.kerscher <alexander.kerscher@muenchen.de>
2025-11-28 09:21:04 +0100

    add svg for qjs

---
+++ frontend/src/components/zaehlung/form/QuerschnittJeStrassenseiteForm.vue
@@ -0,0 +1,488 @@
+<template>
+  <v-sheet
+    width="100%"
+    :height="height"
+    :max-height="height"
+    color="transparent"
+    class="d-flex flex-row justify-center overflow-y-auto"
+  >
+    <svg
+      :height="height"
+      :width="height"
+      viewBox="0 0 1400 1400"
+      preserveAspectRatio="xMidYMid"
+      fill-rule="evenodd"
+      stroke-width="28.222"
+      stroke-linejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+      xml:space="preserve"
+    >
+      <g
+        id="layer1"
+        transform="translate(-13682.197,-17142.499)"
+      >
+        <g
+          id="g5"
+          transform="matrix(0,-0.6666814,0.66728202,0,2306.3104,27427.361)"
+        >
+          <g
+            id="node4-5"
+            transform="translate(577.29494,-599.92464)"
+          >
+            <text
+              id="text22"
+              xml:space="preserve"
+              style="
+                font-style: normal;
+                font-variant: normal;
+                font-weight: normal;
+                font-stretch: normal;
+                font-size: 50.312px;
+                font-family: sans-serif;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                font-variant-numeric: normal;
+                font-variant-east-asian: normal;
+                text-align: start;
+                writing-mode: lr-tb;
+                direction: ltr;
+                text-anchor: start;
+                fill: #000000;
+                stroke-width: 39.1848;
+              "
+              x="19651.174"
+              y="-13382.24"
+              transform="matrix(0,0.97107714,-1.0297843,0,0,0)"
+            >
+              <tspan
+                id="tspan21"
+                x="19651.174"
+                y="-13382.24"
+                transform="translate(0,-2.8480775e-4)"
+                style="stroke-width: 39.1848"
+              >
+                1234567890123
+              </tspan>
+            </text>
+            <text
+              id="text18"
+              xml:space="preserve"
+              style="
+                font-style: normal;
+                font-variant: normal;
+                font-weight: normal;
+                font-stretch: normal;
+                font-size: 50.312px;
+                font-family: sans-serif;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                font-variant-numeric: normal;
+                font-variant-east-asian: normal;
+                text-align: start;
+                writing-mode: lr-tb;
+                direction: ltr;
+                text-anchor: start;
+                fill: #000000;
+                stroke-width: 39.1848;
+              "
+              x="19651.174"
+              y="-13413.685"
+              transform="matrix(0,0.97107715,-1.0297843,0,0,0)"
+            >
+              <tspan
+                id="tspan17"
+                x="19651.174"
+                y="-13413.685"
+                transform="translate(0,-2.8480775e-4)"
+                style="stroke-width: 39.1848"
+              >
+                1234567890123
+              </tspan>
+              <tspan
+                id="tspan18"
+                x="19651.174"
+                y="-13350.795"
+                transform="translate(0,-2.8480775e-4)"
+                style="stroke-width: 39.1848"
+              >
+                4567890123456
+              </tspan>
+            </text>
+          </g>
+          <g
+            id="second_number"
+            transform="matrix(0.79386182,0,0,0.79386182,2963.6448,4304.3997)"
+          >
+            <ellipse
+              id="ellipse13"
+              style="
+                fill: none;
+                fill-opacity: 1;
+                stroke: #000000;
+                stroke-width: 1.93458;
+                stroke-dasharray: none;
+                stroke-opacity: 1;
+              "
+              cx="17797.072"
+              cy="-14376.974"
+              transform="rotate(90)"
+              rx="38.675568"
+              ry="38.710403"
+            />
+            <text
+              id="text14"
+              xml:space="preserve"
+              style="
+                font-size: 63.0885px;
+                text-align: start;
+                writing-mode: lr-tb;
+                direction: ltr;
+                text-anchor: start;
+                fill: #000000;
+                stroke-width: 38.9418;
+                font-family: sans-serif;
+                font-weight: normal;
+                font-style: normal;
+                font-stretch: normal;
+                font-variant: normal;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                font-variant-numeric: normal;
+                font-variant-east-asian: normal;
+              "
+              x="18421.684"
+              y="-13851.564"
+              transform="matrix(0,0.96505433,-1.0362111,0,0,0)"
+            >
+              <tspan id="tspan14">2</tspan>
+            </text>
+          </g>
+          <g
+            id="g22"
+            transform="translate(577.29494,-599.92464)"
+          >
+            <text
+              id="text3"
+              xml:space="preserve"
+              style="
+                font-style: normal;
+                font-variant: normal;
+                font-weight: normal;
+                font-stretch: normal;
+                font-size: 50.312px;
+                font-family: sans-serif;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                font-variant-numeric: normal;
+                font-variant-east-asian: normal;
+                text-align: start;
+                writing-mode: lr-tb;
+                direction: ltr;
+                text-anchor: start;
+                fill: #000000;
+                stroke-width: 39.1848;
+              "
+              x="18441.266"
+              y="-13382.24"
+              transform="matrix(0,0.97107714,-1.0297843,0,0,0)"
+            >
+              <tspan
+                id="tspan15"
+                x="18441.266"
+                y="-13382.24"
+                transform="translate(0,-2.8480775e-4)"
+                style="stroke-width: 39.1848"
+              >
+                1234567890123
+              </tspan>
+            </text>
+            <text
+              id="text20"
+              xml:space="preserve"
+              style="
+                font-style: normal;
+                font-variant: normal;
+                font-weight: normal;
+                font-stretch: normal;
+                font-size: 50.312px;
+                font-family: sans-serif;
+                font-variant-ligatures: normal;
+                font-variant-caps: normal;
+                font-variant-numeric: normal;
+                font-variant-east-asian: normal;
+                text-align: start;
+                writing-mode: lr-tb;
+                direction: ltr;
+                text-anchor: start;
+                fill: #000000;
+                stroke-width: 39.1848;
+              "
+              x="18441.266"
+              y="-13413.685"
+              transform="matrix(0,0.97107715,-1.0297843,0,0,0)"
+            >
+              <tspan
+                id="tspan19"
+                x="18441.266"
+                y="-13413.685"
+                transform="translate(0,-2.8480775e-4)"
+                style="stroke-width: 39.1848"
+              >
+                1234567890123
+              </tspan>
+              <tspan
+                id="tspan20"
+                x="18441.266"
+                y="-13350.795"
+                transform="translate(0,-2.8480775e-4)"
+                style="stroke-width: 39.1848"
+              >
+                4567890123456
+              </tspan>
+            </text>
+          </g>
+          <g
+            id="first_number"
+            transform="matrix(0.79386181,0,0,0.79386183,2963.6448,3633.0195)"
+          >
+            <ellipse
+              id="ellipse3"
+              style="
+                fill: none;
+                fill-opacity: 1;
+                stroke: #000000;
+                stroke-width: 1.93458;
+                stroke-dasharray: none;
+                stroke-opacity: 1;
+              "
+              cx="17797.072"
+              cy="-14376.974"
+              transform="rotate(90)"
+              rx="38.675568"
+              ry="38.710403"
+            />
+            <text
+              id="text4"
+              xml:space="preserve"
+              style="
+                font-size: 63.0885px;
+                text-align: start;
+                writing-mode: lr-tb;
+                direction: ltr;
+                text-anchor: start;
+                fill: #000000;
+                stroke-width: 38.9418;
+              "
+              x="18421.684"
+              y="-13851.564"
+              transform="matrix(0,0.96505433,-1.0362111,0,0,0)"
+            >
+              4
+            </text>
+          </g>
+        </g>
+        <g id="arrows">
+          <g id="g26">
+            <path
+              id="path2-8"
+              style="fill: #000000; stroke-width: 43.1576"
+              d="m 13857.197,17982.499 v -28 h 1050 v 27.997 z"
+              transform="translate(6.4702126e-6,-8.0463116e-5)"
+            />
+            <path
+              id="path24"
+              style="fill: #000000; stroke-width: 78.3672"
+              d="m 15265.992,17469.828 -304.316,176.002 -0.265,-351.547 z"
+              transform="matrix(0.09192953,0,0,0.07964786,13538.802,16577.046)"
+            />
+          </g>
+          <g id="g27">
+            <path
+              id="path1-8"
+              style="fill: #000000; stroke-width: 43.1576"
+              d="m 13857.197,17926.499 v -28 h 1050 v 27.997 z"
+              transform="translate(6.4702126e-6,-8.0463116e-5)"
+            />
+            <path
+              id="path23"
+              style="fill: #000000; stroke-width: 78.3672"
+              d="m 15265.992,17469.828 -304.316,176.002 -0.265,-351.547 z"
+              transform="matrix(-0.09192953,0,0,-0.07964786,15225.592,19303.952)"
+            />
+          </g>
+          <g id="g28">
+            <path
+              id="path2"
+              style="fill: #000000; stroke-width: 43.1577"
+              d="m 13857.197,17786.499 v -28 h 1050 v 27.997 z"
+              transform="translate(6.4702126e-6,-8.0463116e-5)"
+            />
+            <path
+              id="path25"
+              style="fill: #000000; stroke-width: 78.3672"
+              d="m 15265.992,17469.828 -304.316,176.002 -0.265,-351.547 z"
+              transform="matrix(0.09192953,0,0,0.07964786,13538.802,16381.046)"
+            />
+          </g>
+          <g id="g29">
+            <path
+              id="path1"
+              style="fill: #000000; stroke-width: 43.1578"
+              d="m 13857.197,17730.499 v -28 h 1050 v 27.998 z"
+              transform="translate(6.4702126e-6,-8.0463116e-5)"
+            />
+            <path
+              id="path22"
+              style="fill: #000000; stroke-width: 78.3672"
+              d="m 15265.992,17469.828 -304.316,176.002 -0.265,-351.547 z"
+              transform="matrix(-0.09192953,0,0,-0.07964786,15225.592,19107.952)"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+import type ZaehlungDTO from "@/types/zaehlung/ZaehlungDTO";
+
+import { computed, onMounted, ref, watch } from "vue";
+
+import Zaehlart from "@/types/enum/Zaehlart";
+
+interface Props {
+  height: string;
+}
+defineProps<Props>();
+
+const zaehlung = defineModel<ZaehlungDTO>("zaehlung", {
+  required: true,
+});
+const isValid = defineModel<boolean>("isValid", {
+  required: false,
+});
+
+const activeColor = "#D50000";
+const passiveColor = "#9E9E9E";
+
+const selectedKnotenarme = ref<Array<string>>([]);
+
+const availableNodes = computed(() => {
+  return zaehlung.value.knotenarme.map((arm) => `${arm.nummer}`);
+});
+
+onMounted(() => {
+  resetForm();
+});
+
+watch(
+  () => zaehlung.value.knotenarme,
+  () => {
+    resetForm();
+  },
+  { deep: true, immediate: true }
+);
+
+function isNodeAvailable(node: string): boolean {
+  return availableNodes.value.includes(node);
+}
+/**
+ * Wenn für die knotenarm im Array gefunden wurde, wird diese in der Grafik in der "activeColor" dargestellt,
+ * ansonsten in der passiveColor.
+ */
+function calculateColor(knotenarm: string): string | undefined {
+  let color = passiveColor;
+  const gefilteterKnotenarm = selectedKnotenarme.value.filter(
+    (k) => k === knotenarm
+  )[0];
+  if (gefilteterKnotenarm) {
+    color = activeColor;
+  }
+  return color;
+}
+
+function activateNode(knotenarm: string) {
+  if (!selectedKnotenarme.value.includes(knotenarm)) {
+    selectedKnotenarme.value.push(knotenarm);
+  } else {
+    selectedKnotenarme.value.splice(
+      selectedKnotenarme.value.indexOf(knotenarm),
+      1
+    );
+  }
+  validateSelection();
+}
+
+function activateOrDeactivateTotalNode(knotenarm: string) {
+  if (!isNodeAvailable(knotenarm)) {
+    // remove all
+    return;
+  }
+
+  const endings = [];
+
+  if (zaehlung.value.zaehlart === Zaehlart.QU) {
+    endings.push("CL2R");
+    endings.push("CR2L");
+  }
+  if (zaehlung.value.zaehlart === Zaehlart.FJS) {
+    endings.push("LU");
+    endings.push("LD");
+    endings.push("RU");
+    endings.push("RD");
+  }
+
+  const nodeStrings = endings.map((value) => knotenarm + value);
+
+  let nothingRemoved = true;
+
+  nodeStrings.forEach((nodeString) => {
+    if (selectedKnotenarme.value.includes(nodeString)) {
+      nothingRemoved = false;
+      selectedKnotenarme.value.splice(
+        selectedKnotenarme.value.indexOf(nodeString),
+        1
+      );
+    }
+  });
+
+  if (nothingRemoved && isNodeAvailable(knotenarm)) {
+    nodeStrings.forEach((value) => {
+      activateNode(value);
+    });
+  }
+  validateSelection();
+}
+
+function getCursorType(knotenarm: string) {
+  if (isNodeAvailable(knotenarm)) {
+    return `pointer`;
+  } else {
+    return "default";
+  }
+}
+
+function getColorOfNode(knotenarm: string) {
+  if (isNodeAvailable(knotenarm)) {
+    return `#1565C0`;
+  } else {
+    return `#EEEEEE`;
+  }
+}
+
+function resetForm() {
+  selectedKnotenarme.value = [];
+  validateSelection();
+}
+
+function validateSelection() {
+  const selectedKnotenarmNummern = selectedKnotenarme.value.map(
+    (knotenarm: string) => knotenarm.charAt(0)
+  );
+  isValid.value =
+    availableNodes.value.filter(
+      (nodeNumber) => !selectedKnotenarmNummern.includes(nodeNumber)
+    ).length === 0;
+}
+</script>
--- frontend/src/components/zaehlung/form/VerkehrForm.vue
+++ frontend/src/components/zaehlung/form/VerkehrForm.vue
@@ -12,7 +12,12 @@
     v-model:is-valid="isValid"
     :height="height"
   />
-  <!--  TODO für QjS eigene Form-->
+  <querschnitt-je-strassenseite-form
+    v-else-if="zaehlung.zaehlart === Zaehlart.QJS"
+    v-model:zaehlung="zaehlung"
+    v-model:is-valid="isValid"
+    :height="height"
+  />
   <verkehrsbeziehung-kreuzung-form
     v-else
     v-model="zaehlung"
@@ -24,6 +29,7 @@
 import type ZaehlungDTO from "@/types/zaehlung/ZaehlungDTO";
 
 import KnotenverkehrForm from "@/components/zaehlung/form/KnotenverkehrForm.vue";
+import QuerschnittJeStrassenseiteForm from "@/components/zaehlung/form/QuerschnittJeStrassenseiteForm.vue";
 import VerkehrsbeziehungKreisverkehrForm from "@/components/zaehlung/form/VerkehrsbeziehungKreisverkehrForm.vue";
 import VerkehrsbeziehungKreuzungForm from "@/components/zaehlung/form/VerkehrsbeziehungKreuzungForm.vue";
 import Zaehlart from "@/types/enum/Zaehlart";
