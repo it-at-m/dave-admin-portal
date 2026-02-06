48c65d8d6ea0650d4e1ce497b1a1be447cb7577d
ztarbug <markus.zarbock@starwit.de>
2026-02-06 10:36:20 +0100

    add error handling

--- frontend/src/components/config/ConfigApplication.vue
+++ frontend/src/components/config/ConfigApplication.vue
@@ -12,21 +12,26 @@
       variant="flat"
     >
       <v-card-text class="pb-16">
-        <div v-for="(items, category) in groupedConfig" :key="category" class="mb-6">
-          <h3 class="text-h6 mb-3">{{ category.toUpperCase() }}</h3>
-          <v-list>
-            <v-list-item v-for="item in items" :key="item.id">
-              <v-text-field
-                :label="item.keyname"
-                v-model="item.valuefield"
-                variant="outlined"
-                density="compact"
-                class="pt-2"
-                :rules="[validateDatatype(item)]"
-              />
-            </v-list-item>
-          </v-list>
-        </div>
+        <template v-if="configItems.length">
+          <div v-for="(items, category) in groupedConfig" :key="category" class="mb-6">
+            <h3 class="text-h6 mb-3">{{ category.toUpperCase() }}</h3>
+            <v-list>
+              <v-list-item v-for="item in items" :key="item.id">
+                <v-text-field
+                  :label="item.keyname"
+                  v-model="item.valuefield"
+                  variant="outlined"
+                  density="compact"
+                  class="pt-2"
+                  :rules="[validateDatatype(item)]"
+                />
+              </v-list-item>
+            </v-list>
+          </div>
+        </template>
+        <template v-else>
+          <p>Keine Konfigurationselemente gefunden.</p>
+        </template>
       </v-card-text>
       <v-card-actions style="position: absolute; bottom: 0; right: 0">
         <v-spacer></v-spacer>
@@ -45,7 +50,10 @@
 
 import { ref, computed } from 'vue';
 import ConfigurationService from '@/api/service/ConfigurationService';
+import { useSnackbarStore } from "@/store/SnackbarStore";
 import type ConfigurationItemDTO from '@/types/configuration/ConfigurationItemDTO';
+
+const snackbarStore = useSnackbarStore();
 
 const configItems = ref<ConfigurationItemDTO[]>([]);
 
@@ -80,12 +88,16 @@ loadConfigValues();
 function loadConfigValues() {
   ConfigurationService.getAllConfigurationValues().then((response) => {
     configItems.value = response;
+  }).catch((error) => {
+    snackbarStore.showError('Fehler beim Laden der Konfiguration', error);
   });
 }
 
 function saveConfigValues() {
   ConfigurationService.saveConfiguration(configItems.value).then(() => {
     loadConfigValues();
+  }).catch((error) => {
+    snackbarStore.showError('Fehler beim Speichern der Konfiguration', error);
   });
 }
 
